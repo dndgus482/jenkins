dee856a57b9e9d57a3d9777b83c81d6b096fdcd3
woonghyun.ka <dndgus4820@gmail.com>
2023-02-20 16:18:18 +0900

    aaaa

---
+++ mvn.groovy
@@ -0,0 +1,31 @@
+properties([parameters([string(defaultValue: 'test', description: 'test or verify', name: 'testType', trim: false)])])
+
+node {
+    stage("git") {
+        git url: 'https://github.com/hyunil-shin/java-maven-junit-helloworld.git', branch: 'failed'
+
+    }
+    
+    stage('build') {
+        withEnv(["PATH+MAVEN=${tool 'mvn-3.6.0'}/bin"]) {
+            sh 'mvn --version'
+            try {
+                sh "mvn clean ${params.testType}"
+            } catch(e) {
+                currentBuild.result = 'FAILURE'
+            }
+
+        }
+    }
+    
+    stage('report') {
+        junit 'target/surefire-reports/*.xml'
+        jacoco execPattern: 'target/**.exec'
+        archiveArtifacts artifacts: 'target/surefire-reports/*.xml'
+        if (currentBuild.result == 'FAILURE') {
+            emailext body: 'Test Message', subject: 'Test Subject', to: 'woonghyun.ka@nhnpayco.com'
+        }
+
+
+    }
+}
